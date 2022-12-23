e1ca3026e18eac625480d0ca56f280ae073dde13
Gianluca Lucchesi <lucchesi.gianluca@hotmail.com>
2022-12-23 13:50:03 +0100

    AOC 2022 Day 4 - Part 1 & 2

--- src/com/year22/day4/Day4.java
+++ src/com/year22/day4/Day4.java
@@ -5,16 +5,17 @@ public class Day4 {
         String[] input = ReadFile.getInput();
         int fullyContained = 0, overlaps = 0;
 
-        // Part 1 & 2
         for (String pair : input) {
             String[] parts = pair.split(",");
             int[] elf1 = {Integer.parseInt(parts[0].split("-")[0]), Integer.parseInt(parts[0].split("-")[1])};
             int[] elf2 = {Integer.parseInt(parts[1].split("-")[0]), Integer.parseInt(parts[1].split("-")[1])};
 
+            // Part 1
             if ((elf1[0] >= elf2[0] && elf1[1] <= elf2[1]) || (elf2[0] >= elf1[0] && elf2[1] <= elf1[1])) {
                 fullyContained++;
             }
 
+            // Part 2
             if ((elf1[0] >= elf2[0] && elf1[0] <= elf2[1]) || (elf1[1] >= elf2[0] && elf1[1] <= elf2[1])
                     || (elf2[0] >= elf1[0] && elf2[0] <= elf1[1]) || (elf2[1] >= elf1[0] && elf2[1] <= elf1[1])) {
                 overlaps++;
